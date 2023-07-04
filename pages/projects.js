92c000c78910a9fdc0fde0138cdbd58d813415b9
Siddharth Lohani <me@siddharthlohani.dev>
2023-07-04 06:53:00 -0400

    added some projects

--- pages/projects.js
+++ pages/projects.js
@@ -1,5 +1,51 @@
 export default function Projects() {
     return (
-        <p>Coming soon!</p>
+        <>
+            <h1>Featured Projects</h1>
+
+
+            <div class="flex-grid-thirds">
+                <button class="col">
+                    <h3>Genesis Grade Alerts</h3>
+                    <p>A simple Python application which notifies users when their grades are updated in the Genesis Parent Portal via SMS, either through Gmail's SMTP server or via Twilio's SMS API.</p>
+                </button>
+                <button class="col">
+                    <h3>NJ Covid Alerts</h3>
+                    <p>Coronavirus tracker for the state of New Jersey. Built with Selenium, implemented in Python. Seeing that cases keep rising, it's important to be aware of the new developments in cases, vaccines, and tests.</p>
+                </button>
+                <button class="col">
+                    <h3>2020 Election Result Tweeter</h3>
+                    <p>The 2020 election was a rough one, with historic voter turnout. This is a Python Web Scraper which tweeted live election data from ABC on my personal Twitter account.</p>
+                </button>
+            </div>
+            <div class="flex-grid-thirds">
+                <button class="col">
+                    <h3>iMessage Exporter</h3>
+                    <p>A Command Line Interface (CLI) that analyzes and exports iMessage conversations into .csv files containing messages, timestamps, and IDs 📲</p>
+                </button>
+                <button class="col">
+                    <h3>title</h3>
+                    <p>description</p>
+                </button>
+                <button class="col">
+                    <h3>title</h3>
+                    <p>description</p>
+                </button>
+            </div>
+            <div class="flex-grid-thirds">
+                <button class="col">
+                    <h3>title</h3>
+                    <p>description</p>
+                </button>
+                <button class="col">
+                    <h3>title</h3>
+                    <p>description</p>
+                </button>
+                <button class="col">
+                    <h3>title</h3>
+                    <p>description</p>
+                </button>
+            </div>
+        </>
     );
 }
